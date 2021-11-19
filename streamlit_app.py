42ac02a656f008fd6ba072c3f6f82c1e8d4cc1dc
Jessica Smith <jessicasmith@Jessicas-MacBook-Pro.local>
2021-11-18 19:42:53 -0800

    added a few things

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,17 @@
 import streamlit as st
 
-st.title("Hackathon Pumpkin")
+st.title("Hackathon Pumpkin 🎃")
 
 click = st.button("push me")
 if click:
     st.balloons()
 
-select = st.radio("classify pumpkin",["Squash","Gourd"])
-st.write("You chose",select)
+select = st.sidebar.radio("classify pumpkin",["Squash","Gourd"])
+st.sidebar.write("You chose",select)
 
 check = st.checkbox("Have a happy holiday",["I will"])
+if check:
+    st.write("I will, thanks")
+
+with st.expander("Learn more about this project"):
+    st.write("This is an experimental practice project to get familiar with simple Streamlit widgets")
